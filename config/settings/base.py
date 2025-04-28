2c820fd704457b8e33ffe00fe60a5670d1901e5d
Paul Bryant <paulwilliambryant@gmail.com>
2025-04-28 12:43:35 -0700

    update base settings to use defaul db.qlite name

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'start_of_breeding_season_2024.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
